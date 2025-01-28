a715239737c0245a84b0ff19a73cc9ea78afec36
AtharvaGhorpade2405 <atharvasg24@gmail.com>
2025-01-28 07:49:51 +0530

    changed api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,5 @@
 const API_BASE_URL = "https://movie-monkey-server-1.onrender.com";
+const BACKEND_API_KEY = import.meta.env.VITE_BACKEND_API_KEY;
 
 export const getPopularMovies = async () => {
   const url = `${API_BASE_URL}/movies/popular`; // Call your backend server
@@ -6,6 +7,7 @@ export const getPopularMovies = async () => {
     method: "GET",
     headers: {
       accept: "application/json",
+      'x-api-key': BACKEND_API_KEY,
     },
   };
 
@@ -16,11 +18,12 @@ export const getPopularMovies = async () => {
 
 
 export const getMovies = async (searchQuery) => {
-  const url = `${API_BASE_URL}/movies/search?query=${searchQuery}`;
+  const url = `${API_BASE_URL}/movies/search?query=${encodeURIComponent(searchQuery)}`;
   const options = {
     method: "GET",
     headers: {
       accept: "application/json",
+      'x-api-key': BACKEND_API_KEY,
     },
   };
 
